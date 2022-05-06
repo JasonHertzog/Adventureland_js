5d9b21d10c940dd77a2885e7b3f424303fe57fd7
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-05-06 12:24:32 -0400

    Update Base.100.js
    Improved use_all_loops() to allow for a second argument, which is used to define the focus variable in check_hp_and_mp()

--- Base/Base.100.js
+++ Base/Base.100.js
@@ -1,3 +1,15 @@
+// Set defaults to use, if desired, for the use_all_loops function.
+// functions can be customized by changing their defaults arguments.
+
+// The second argument sets check_hp_and_mp code focus to hp/mp/both
+function use_all_loops(debug = true, check_hp_and_mp_focus = "both"){
+    // Looting happens every tick.
+    loot();
+    // Uses HP & MP Manager every tick.
+    // debug, focus (hp/mp/both), hp tolerance, mp tolerance, ratio for focus.
+    check_hp_and_mp(debug, check_hp_and_mp_focus, 0.85, 0.2, 3);
+
+}
 
 function load_all_code(debug = true){
     if(debug == true){game_log("Loaded all code!", "gray")}
@@ -5,12 +17,4 @@ function load_all_code(debug = true){
     load_code("HP_MP_Manager");
     // Server Hopping Script
     load_code("Realm_Hopper");
-}
-
-function use_all_loops(debug = true){
-    // Looting happens every tick.
-    loot();
-    // Uses HP & MP Manager every tick.
-    check_hp_and_mp(debug, "both");
-
 }
